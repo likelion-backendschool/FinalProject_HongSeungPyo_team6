2bf364eadf94dc004d04ab6ed94d8c316168ea1c
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-10-17 13:21:17 +0900

    초기 세팅

---
+++ 1Week_Mission/src/main/java/NWeek_Mission/Week_Mission/HomeController.java
@@ -0,0 +1,14 @@
+package NWeek_Mission.Week_Mission;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    @ResponseBody
+    public String main(){
+        return "안녕";
+    }
+}
