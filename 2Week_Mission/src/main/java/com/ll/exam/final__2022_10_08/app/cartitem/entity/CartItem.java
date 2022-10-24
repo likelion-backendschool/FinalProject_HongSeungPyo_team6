217c875e676284e440df73d01d510e592a72808d
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-10-24 11:42:05 +0900

    ERO 설계 2

--- 2Week_Mission/src/main/java/com/ll/exam/final__2022_10_08/app/cartitem/entity/CartItem.java
+++ 2Week_Mission/src/main/java/com/ll/exam/final__2022_10_08/app/cartitem/entity/CartItem.java
@@ -1,6 +1,9 @@
 package com.ll.exam.final__2022_10_08.app.cartitem.entity;
 
 import com.ll.exam.final__2022_10_08.app.base.entity.BaseEntity;
+import com.ll.exam.final__2022_10_08.app.member.entity.Member;
+import com.ll.exam.final__2022_10_08.app.postkeyword.entity.PostKeyword;
+import com.ll.exam.final__2022_10_08.app.product.entity.Product;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,7 +11,9 @@ import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
+import static javax.persistence.FetchType.LAZY;
 import static lombok.AccessLevel.PROTECTED;
 
 @Entity
@@ -18,4 +23,8 @@ import static lombok.AccessLevel.PROTECTED;
 @ToString(callSuper = true)
 @NoArgsConstructor(access = PROTECTED)
 public class CartItem extends BaseEntity {
+    @ManyToOne(fetch = LAZY)
+    private Member member;
+    @ManyToOne(fetch = LAZY)
+    private Product product;
 }
--- 2Week_Mission/src/main/java/com/ll/exam/final__2022_10_08/app/mybook/entity/MyBook.java
+++ 2Week_Mission/src/main/java/com/ll/exam/final__2022_10_08/app/mybook/entity/MyBook.java
@@ -1,6 +1,8 @@
 package com.ll.exam.final__2022_10_08.app.mybook.entity;
 
 import com.ll.exam.final__2022_10_08.app.base.entity.BaseEntity;
+import com.ll.exam.final__2022_10_08.app.member.entity.Member;
+import com.ll.exam.final__2022_10_08.app.product.entity.Product;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,7 +10,9 @@ import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
+import static javax.persistence.FetchType.LAZY;
 import static lombok.AccessLevel.PROTECTED;
 
 @Entity
@@ -18,4 +22,9 @@ import static lombok.AccessLevel.PROTECTED;
 @ToString(callSuper = true)
 @NoArgsConstructor(access = PROTECTED)
 public class MyBook extends BaseEntity {
+    @ManyToOne(fetch = LAZY)
+    private Member member;
+
+    @ManyToOne(fetch = LAZY)
+    private Product product;
 }
